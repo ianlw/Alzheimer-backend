c0da6da596c4c2ff57710972530e784b8f6f9222
ianlw <iancillonn@gmail.com>
2025-10-17 00:06:53 -0500

    feat: habilitar CORS para permitir peticiones desde cualquier frontend

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routers import usuarios, pacientes
+from fastapi.middleware.cors import CORSMiddleware # <--- 1. Importa el middleware
 
 # Crea las tablas en la base de datos (si no existen)
 # Esta línea es crucial para que SQLAlchemy cree tu archivo .db y las tablas
@@ -11,6 +12,16 @@ app = FastAPI(
     title="API para Detección de Alzheimer",
     description="Backend para gestionar médicos, pacientes y análisis de MRI.",
     version="1.0.0"
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"], # Permite todos los encabezados
 )
 
 # Incluir los routers desde la carpeta 'routers'
